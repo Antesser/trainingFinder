17b8dc6933a2f295ccd45ac395a3d356d2bfb47a
Antesser <ahriman.kun@gmail.com>
2026-09-13 09:40:00 +0300

    Fixed repo 2

--- internal/repository/booking/repository.go
+++ internal/repository/booking/repository.go
@@ -67,6 +67,9 @@ func (r *repository) ListBookings(ctx context.Context, data model.ListBookingsRe
 	if data.Filter.BookedFrom != nil {
 		qb = qb.Where("book_from = ?", data.Filter.BookedFrom)
 	}
+	if data.Filter.BookedFrom != nil {
+		qb = qb.Where("booked_by = ?", data.Filter.BookedBy)
+	}
 	if data.Filter.BookedTo != nil {
 		qb = qb.Where("book_to = ?", data.Filter.BookedTo)
 	}
