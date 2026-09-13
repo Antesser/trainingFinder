c80b06651cbe8395dbbaa926aae5fb02f71b2c54
Antesser <ahriman.kun@gmail.com>
2026-09-13 09:10:16 +0300

    fixed tables

--- migrations/20260719073043_booking.sql
+++ migrations/20260719073043_booking.sql
@@ -1,14 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS training_booking (
-                                        id TEXT,
-                                        training_id TEXT NOT NULL,
-                                        booked_by UUID NOT NULL,
-                                        created_at TIMESTAMP  NOT NULL,
-    status text,
-                                        book_from TIMESTAMP NOT NULL,
-                                        book_to TIMESTAMP NOT NULL,
-                                        PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS training_booking
+(
+    id          TEXT,
+    training_id TEXT      NOT NULL,
+    booked_by   UUID      NOT NULL,
+    created_at  TIMESTAMP NOT NULL,
+    status      text,
+    book_from   TIMESTAMP NOT NULL,
+    book_to     TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
 );
 -- +goose StatementEnd
 
